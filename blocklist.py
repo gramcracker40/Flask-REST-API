50112ed7acb0784d1def0570224c6bc63522f2f1
Garrett Mathers <42726625+gramcracker40@users.noreply.github.com>
2023-02-27 11:08:26 -0600

    Update blocklist.py

--- blocklist.py
+++ blocklist.py
@@ -1,6 +1,6 @@
 """
 Contains the blocklist of the application. Meaning any JWT's that are created are
-stored here in this file
+stored here in this file, needs transition to database table.
 
 """
 
